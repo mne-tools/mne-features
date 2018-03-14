96010f8980f882f7262a6c6d91cd4ee415feefc0
Jean-Baptiste Schiratti <jbschiratti@tsilinuxb49.enst.fr>
2018-03-14 09:57:56 +0100

    - Tests for univariate + bivariate feature functions. - Added test data.

--- mne_features/tests/test_bivariate.py
+++ mne_features/tests/test_bivariate.py
@@ -3,16 +3,24 @@
 # License: BSD 3 clause
 
 
+import mne
 import numpy as np
+import os.path as op
 from numpy.testing import assert_equal
 from mne_features.bivariate import compute_max_cross_correlation
 
 
+path_data = op.join(op.dirname(__file__), 'data', 'test_data_chbmit-epo.fif')
+epochs = mne.read_epochs(path_data)
+data = epochs.get_data()
+n_epochs, n_channels = data.shape[:2]
+sfreq = epochs.info['sfreq']
+rng = np.random.RandomState(42)
+
+
 def test_max_cross_corr():
-    rng = np.random.RandomState(42)
-    X = rng.standard_normal((10, 256))
-    Xnew = compute_max_cross_correlation(256., X)
-    assert_equal(Xnew.shape, (55,))
+    Xnew = compute_max_cross_correlation(sfreq, data[0, :, :])
+    assert_equal(Xnew.shape, (n_channels * (n_channels + 1) / 2,))
 
 
 if __name__ == '__main__':
--- mne_features/tests/test_univariate.py
+++ mne_features/tests/test_univariate.py
@@ -2,24 +2,91 @@
 #         Alexandre Gramfort <alexandre.gramfort@inria.fr>
 # License: BSD 3 clause
 
+
+import mne
 import numpy as np
-from numpy.testing import assert_equal
-from mne_features.univariate import (compute_kurtosis, compute_mean,
+import os.path as op
+from numpy.testing import assert_equal, assert_almost_equal
+from scipy import signal
+from mne_features.univariate import (_slope_lstsq, compute_mean,
+                                     compute_variance, compute_std,
                                      compute_ptp, compute_skewness,
-                                     compute_variance)  # noqa
+                                     compute_kurtosis, compute_hurst_exponent,
+                                     compute_app_entropy, power_spectrum,
+                                     compute_samp_entropy, compute_decorr_time,
+                                     compute_power_spectrum_freq_bands,
+                                     compute_spect_hjorth_mobility,
+                                     compute_spect_hjorth_complexity,
+                                     compute_hjorth_mobility,
+                                     compute_hjorth_complexity,
+                                     compute_higuchi_fd, compute_katz_fd)
 
 
-def test_univariate():
-    N, C, T = 3, 5, 30
-    rng = np.random.RandomState(42)
-    data = rng.randn(N, C, T)
-    for func in (compute_kurtosis, compute_mean,
-                 compute_ptp, compute_skewness,
-                 compute_variance):
-        X = func(data)
-        assert_equal(X.shape, (N, C))
+path_data = op.join(op.dirname(__file__), 'data', 'test_data_chbmit-epo.fif')
+epochs = mne.read_epochs(path_data)
+data = epochs.get_data()
+n_epochs, n_channels = data.shape[:2]
+sfreq = epochs.info['sfreq']
+rng = np.random.RandomState(42)
+
+
+def test_slope_lstsq():
+    x = rng.standard_normal((100,))
+    m = rng.uniform()
+    y = m * x + 1
+    s1 = _slope_lstsq(x, y)
+    s2 = np.polyfit(x, y, 1)[0]
+    assert_almost_equal(s1, m)
+    assert_almost_equal(s1, s2)
+
+
+def test_shape_output():
+    for func in (compute_mean, compute_variance, compute_std,
+                 compute_kurtosis, compute_skewness, compute_ptp,
+                 compute_hurst_exponent, compute_app_entropy,
+                 compute_samp_entropy, compute_hjorth_complexity,
+                 compute_hjorth_mobility, compute_higuchi_fd, compute_katz_fd):
+        for j in range(n_epochs):
+            feat = func(data[j, :, :])
+            assert_equal(feat.shape, (n_channels,))
+
+
+def test_shape_output_decorr_time():
+    for j in range(n_epochs):
+        feat = compute_decorr_time(sfreq, data[j, :, :])
+        assert_equal(feat.shape, (n_channels,))
+
+
+def test_shape_output_power_spectrum_freq_bands():
+    fb = np.array([0.1, 4, 8, 12, 30])
+    n_freqs = fb.shape[0]
+    for j in range(n_epochs):
+        feat = compute_power_spectrum_freq_bands(sfreq, fb, data[j, :, :])
+        assert_equal(feat.shape, (n_channels * (n_freqs - 1),))
+
+
+def test_power_spectrum():
+    for j in range(n_epochs):
+        ps, freqs = power_spectrum(sfreq, data[j, :, :], return_db=False)
+        assert_almost_equal(np.mean(data[j, :, :] ** 2), np.sum(ps))
+
+
+def test_shape_output_spect_hjorth_mobility():
+    for j in range(n_epochs):
+        feat = compute_spect_hjorth_mobility(sfreq, data[j, :, :])
+        assert_equal(feat.shape, (n_channels,))
+
+
+def test_shape_output_spect_hjorth_complexity():
+    for j in range(n_epochs):
+        feat = compute_spect_hjorth_complexity(sfreq, data[j, :, :])
+        assert_equal(feat.shape, (n_channels,))
 
 
 if __name__ == '__main__':
 
-    test_univariate()
+    test_slope_lstsq()
+    test_shape_output()
+    test_shape_output_decorr_time()
+    test_shape_output_power_spectrum_freq_bands()
+    test_power_spectrum()
