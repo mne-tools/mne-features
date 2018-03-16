bf3e678ffb6e83de22119376316a60bec4a0e002
Jean-Baptiste Schiratti <jbschiratti@tsilinuxb49.enst.fr>
2018-03-16 12:57:39 +0100

    Fixed __init__.py.

--- mne_features/__init__.py
+++ mne_features/__init__.py
@@ -18,17 +18,4 @@
 
 __version__ = '0.1.dev0'
 
-from .univariate import (compute_mean, compute_variance, compute_std,
-                         compute_ptp, compute_skewness, compute_kurtosis,
-                         compute_hurst_exponent, compute_app_entropy,
-                         compute_samp_entropy, compute_decorr_time,
-                         power_spectrum, compute_power_spectrum_freq_bands,
-                         compute_spect_hjorth_mobility,
-                         compute_spect_hjorth_complexity,
-                         compute_hjorth_mobility, compute_hjorth_complexity,
-                         compute_higuchi_fd, compute_katz_fd)
-
-from .bivariate import (compute_max_cross_correlation,
-                        compute_nonlinear_interdep,
-                        compute_phase_locking_value, compute_spect_corr_coefs,
-                        compute_time_corr_coefs)
+from . import univariate, bivariate
