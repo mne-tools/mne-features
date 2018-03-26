5c713ec96ead4c5928a28978e3ce6455b1dd1bb2
Jean-Baptiste Schiratti <jbschiratti@tsilinuxb49.enst.fr>
2018-03-26 16:53:50 +0200

    Added example of classification using MNE-Features.

--- examples/plot_mne_sample_features.py
+++ examples/plot_mne_sample_features.py
@@ -18,17 +18,24 @@ Proc. IEEE ICASSP Conf. 2018
 # License: BSD 3 clause
 
 import mne
+import numpy as np
 from mne.datasets import sample
-from mne_features.univariate import compute_kurtosis
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_score, KFold
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+from mne_features.feature_extraction import extract_features
 
 print(__doc__)
 
 ###############################################################################
-# Generate sample EEG data
+# Let us import the data using MNE-Python and epoch it:
+
 data_path = sample.data_path()
 raw_fname = data_path + '/MEG/sample/sample_audvis_filt-0-40_raw.fif'
 event_fname = data_path + '/MEG/sample/sample_audvis_filt-0-40_raw-eve.fif'
-tmin, tmax = -0.1, 0.4
+tmin, tmax = -0.2, 0.5
 event_id = dict(aud_l=1, vis_l=3)
 
 # Setup for reading the raw data
@@ -46,11 +53,24 @@ labels = epochs.events[:, -1]
 data = epochs.get_data()
 
 ###############################################################################
-# Extract some features to classify epochs
+# Prepare for the classification task:
 
-X = compute_kurtosis(data)
+rs = 42
+pipe = Pipeline([('scaler', StandardScaler()),
+                 ('lr', LogisticRegression(random_state=rs))])
+y = labels
 
 ###############################################################################
-# Run classification
+# Classification using features (mean, peak-to-peak amplitude,
+# standard deviation) computed using MNE-Features:
 
-# XXX TODO
+selected_funcs = {'mean', 'ptp_amplitude', 'std'}
+X_new = extract_features(data, raw.info['sfreq'], selected_funcs)
+kf = KFold(n_splits=3, random_state=rs)
+scores = cross_val_score(pipe, X_new, y, scoring='accuracy', cv=kf)
+
+###############################################################################
+# Print the cross-validation score:
+
+print('Cross-validation accuracy score = %s (+/- %s)' % (np.mean(scores),
+                                                         np.std(scores)))
