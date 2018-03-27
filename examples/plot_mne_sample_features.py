f86f5b8918cf76499a860bb45dbb309ca7f0655a
Jean-Baptiste SCHIRATTI <jean.baptiste.schiratti@gmail.com>
2018-03-27 07:25:36 +0200

    Examples (#10)
    * Added example of classification using MNE-Features.
    
    * Improved doc (description of MNE-Features + dependencies).
    
    * fix example + docstrings

--- examples/plot_mne_sample_features.py
+++ examples/plot_mne_sample_features.py
@@ -1,15 +1,21 @@
 """
-=====================================
-Extract features from MEG time series
-=====================================
+========================================================
+Extract features from MEG time series for classification
+========================================================
 
-The example is based on the method proposed in:
+The code for this example is based on the method proposed in:
 
 Jean-Baptiste SCHIRATTI, Jean-Eudes LE DOUGET, Michel LE VAN QUYEN,
 Slim ESSID, Alexandre GRAMFORT,
 "An ensemble learning approach to detect epileptic seizures from long
 intracranial EEG recordings"
 Proc. IEEE ICASSP Conf. 2018
+
+.. note::
+
+    This example is for illustration purposes, as other methods
+    may lead to better performance on such a dataset (classification
+    of auditory vs. visual stimuli).
 
 """  # noqa
 
@@ -18,39 +24,59 @@ Proc. IEEE ICASSP Conf. 2018
 # License: BSD 3 clause
 
 import mne
+import numpy as np
 from mne.datasets import sample
-from mne_features.univariate import compute_kurtosis
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_score, KFold
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+from mne_features.feature_extraction import extract_features
 
 print(__doc__)
 
 ###############################################################################
-# Generate sample EEG data
+# Let us import the data using MNE-Python and epoch it:
+
 data_path = sample.data_path()
 raw_fname = data_path + '/MEG/sample/sample_audvis_filt-0-40_raw.fif'
 event_fname = data_path + '/MEG/sample/sample_audvis_filt-0-40_raw-eve.fif'
-tmin, tmax = -0.1, 0.4
+tmin, tmax = -0.2, 0.5
 event_id = dict(aud_l=1, vis_l=3)
 
 # Setup for reading the raw data
 raw = mne.io.read_raw_fif(raw_fname, preload=True)
 raw.filter(.5, None, fir_design='firwin')
 events = mne.read_events(event_fname)
-picks = mne.pick_types(raw.info, meg=True, eeg=False)
+picks = mne.pick_types(raw.info, meg='grad', eeg=False)
 
 # Read epochs
 epochs = mne.Epochs(raw, events, event_id, tmin, tmax, picks=picks, proj=True,
-                    decim=2, baseline=None, preload=True)
+                    baseline=None, preload=True)
 labels = epochs.events[:, -1]
 
 # get MEG and EEG data
 data = epochs.get_data()
 
 ###############################################################################
-# Extract some features to classify epochs
+# Prepare for the classification task:
 
-X = compute_kurtosis(data)
+pipe = Pipeline([('scaler', StandardScaler()),
+                 ('lr', LogisticRegression(random_state=42))])
+y = labels
 
 ###############################################################################
-# Run classification
+# Classification using features (mean, peak-to-peak amplitude,
+# standard deviation). See :ref:`api_documentation` for full list of supported
+# features.
 
-# XXX TODO
+selected_funcs = {'mean', 'ptp_amplitude', 'std'}
+X_new = extract_features(data, raw.info['sfreq'], selected_funcs)
+kf = KFold(n_splits=3, random_state=42)
+scores = cross_val_score(pipe, X_new, y, scoring='accuracy', cv=kf)
+
+###############################################################################
+# Print the cross-validation score:
+
+print('Cross-validation accuracy score = %1.3f (+/- %1.5f)' % (np.mean(scores),
+                                                               np.std(scores)))
