* [WIP] Adding new tests for univariate feature functions * Fixed buggy compute_zero_crossings * Added normalization in compute_line_length.

mne_features/tests/test_univariate.py
@@ -4,6 +4,7 @@
 
 
 import numpy as np
+from math import sqrt
 from numpy.testing import assert_equal, assert_almost_equal, assert_raises
 
 from mne_features.univariate import (_slope_lstsq, compute_mean,
@@ -30,9 +31,10 @@ from mne_features.univariate import (_slope_lstsq, compute_mean,
 
 rng = np.random.RandomState(42)
 sfreq = 256.
+data1 = np.array([[0., 0., 2., -2., 0., -1., -1., 0.],
+                  [1., 1., -1., -1., 0., 1., 1., 0.]])
 data = rng.standard_normal((10, 20, int(sfreq)))
 n_epochs, n_channels = data.shape[:2]
-n_times = data[0].shape[1]
 
 
 def test_slope_lstsq():
@@ -45,12 +47,49 @@ def test_slope_lstsq():
     assert_almost_equal(s1, s2)
 
 
+def test_mean():
+    expected = np.array([-1 / 4, 1 / 4])
+    assert_almost_equal(compute_mean(data1), expected)
+
+
+def test_variance():
+    expected = np.array([19 / 14, 11 / 14])
+    assert_almost_equal(compute_variance(data1), expected)
+
+
+def test_std():
+    expected = np.array([sqrt(19 / 14), sqrt(11 / 14)])
+    assert_almost_equal(compute_std(data1), expected)
+
+
+def test_skewness():
+    expected = np.array([42 / (19 * sqrt(19)), -18 / (11 * sqrt(11))])
+    assert_almost_equal(compute_skewness(data1), expected)
+
+
+def test_kurtosis():
+    expected = np.array([1141 / 361, 197 / 121])
+    assert_almost_equal(compute_kurtosis(data1), expected)
+
+
+def test_ptp_amp():
+    expected = np.array([4, 2])
+    assert_almost_equal(compute_ptp_amp(data1), expected)
+
+
+def test_line_length():
+    expected = np.array([10 / 7, 5 / 7])
+    assert_almost_equal(compute_line_length(data1), expected)
+
+
+def test_zero_crossings():
+    expected = np.array([5, 3])
+    assert_almost_equal(compute_zero_crossings(data1), expected)
+
+
 def test_shape_output():
-    for func in (compute_mean, compute_variance, compute_std,
-                 compute_kurtosis, compute_skewness, compute_ptp_amp,
-                 compute_hurst_exp, compute_hjorth_complexity,
+    for func in (compute_hurst_exp, compute_hjorth_complexity,
                  compute_hjorth_mobility, compute_higuchi_fd, compute_katz_fd,
-                 compute_zero_crossings, compute_line_length,
                  compute_svd_entropy, compute_svd_fisher_info):
         for j in range(n_epochs):
             feat = func(data[j, :, :])
@@ -131,6 +170,14 @@ def test_shape_output_teager_kaiser_energy():
 if __name__ == '__main__':
 
     test_slope_lstsq()
+    test_mean()
+    test_variance()
+    test_std()
+    test_skewness()
+    test_kurtosis()
+    test_ptp_amp()
+    test_line_length()
+    test_zero_crossings()
     test_shape_output()
     test_shape_output_decorr_time()
     test_shape_output_pow_freq_bands()

mne_features/univariate.py
@@ -648,6 +648,29 @@ def compute_katz_fd(data):
     return katz
 
 
+@nb.jit([nb.float64[:](nb.float64[:, :]), nb.float32[:](nb.float32[:, :])],
+        nopython=True)
+def _zero_crossings(data):
+    """Utility function for :func:`compute_zero_crossings`.
+
+    Parameters
+    ----------
+    data : ndarray, shape (n_channels, n_times)
+
+    Returns
+    -------
+    output : ndarray, shape (n_channels,)
+    """
+    n_channels, n_times = data.shape
+    zc = np.zeros((n_channels,), dtype=data.dtype)
+    for j in range(n_channels):
+        for i in range(n_times - 1):
+            if data[j, i] == 0 or data[j, i] * data[j, i + 1] < 0:
+                zc[j] += 1
+        zc[j] += int(data[j, n_times - 1] == 0)
+    return zc
+
+
 def compute_zero_crossings(data):
     """Number of zero crossings (per channel).
 
@@ -663,7 +686,7 @@ def compute_zero_crossings(data):
     -----
     Alias of the feature function: **zero_crossings**
     """
-    return np.sum(np.diff(np.sign(data), axis=-1) != 0, axis=-1)
+    return _zero_crossings(data)
 
 
 def compute_line_length(data):
@@ -689,7 +712,7 @@ def compute_line_length(data):
                  International Conference of the IEEE (Vol. 2, pp. 1707-1710).
                  IEEE.
     """
-    return np.sum(np.abs(np.diff(data, axis=-1)), axis=-1)
+    return np.mean(np.abs(np.diff(data, axis=-1)), axis=-1)
 
 
 def compute_spect_entropy(sfreq, data):